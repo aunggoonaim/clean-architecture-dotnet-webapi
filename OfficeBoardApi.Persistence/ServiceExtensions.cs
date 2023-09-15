80ecb7b21f20540954b0a5ffb6e4a5e01311f3ea
aunggoonaim <rabbit.dev.group@gmail.com>
2023-09-15 23:57:49 +0700

    add setting

--- OfficeBoardApi.Persistence/ServiceExtensions.cs
+++ OfficeBoardApi.Persistence/ServiceExtensions.cs
@@ -4,6 +4,7 @@ using OfficeBoardApi.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OfficeBoardApi.Domain.Setting;
 
 namespace OfficeBoardApi.Persistence;
 
@@ -12,9 +13,20 @@ public static class ServiceExtensions
     public static void ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Sqlite");
+
+        if (connectionString is null)
+        {
+            throw new Exception("Not found Sqlite connection string.");
+        }
+
         services.AddDbContext<DataContext>(opt => opt.UseSqlite(connectionString));
+
+        services.Configure<AppSetting>(configuration.GetSection("AppSettings"));
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IUserRepository, UserRepository>();
+
+        services.AddHealthChecks();
     }
 }
